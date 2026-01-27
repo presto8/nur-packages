dbc2e8e75072f1797b0e7da624dafd380bbae685
Preston Hunt <me@prestonhunt.com>
2026-01-27 07:33:36 -0800

    update pscripts to latest version

--- pkgs/pscripts/default.nix
+++ pkgs/pscripts/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation(finalAttrs: rec {
   pname = "pscripts";
-  version = "2026-01-08";
+  version = "2026-01-26";
 
   src = fetchFromGitHub {
     owner = "presto8";
     repo = "pscripts";
     rev = "v${version}";
-    hash = "sha256-3jQuNMQi5H87SndeZFSPUcn5rMEYisc+3O7jzOItpSc=";
+    hash = "sha256-oze614H9Tq+LEIoxn0fk8z825KMyi5zTOUzJ49tKNZA=";
   };
 
   dontConfigure = true;
