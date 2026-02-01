0b6bf095b83a668e8a0a6319b18064ddc5c44cd5
Preston Hunt <me@prestonhunt.com>
2026-02-01 08:11:00 -0800

    bump version

--- pkgs/pscripts/default.nix
+++ pkgs/pscripts/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation(finalAttrs: rec {
   pname = "pscripts";
-  version = "2026-01-26";
+  version = "2026-02-01";
 
   src = fetchFromGitHub {
     owner = "presto8";
     repo = "pscripts";
     rev = "v${version}";
-    hash = "sha256-oze614H9Tq+LEIoxn0fk8z825KMyi5zTOUzJ49tKNZA=";
+    hash = "sha256-CGadn7hzhlRaSzd+y5+HZtBLTgubYb07Dwq93VQU/rI=";
   };
 
   dontConfigure = true;
